8e565047197d386254f8b83ff0b783f2a6555eb1
ShivamMukherjee <mshivam98@gmail.com>
2017-07-21 11:50:20 +0530

    Added several activation functions for usage.

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,4 @@
-from numpy import exp, array, random, dot
+from numpy import exp, array, random, dot, tanh
 
 train_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
 train_outputs = array([[0, 1, 0, 1]]).T
@@ -12,42 +12,80 @@ random.seed(1)
 synaptic_weights = 2 * random.random((3, 1)) - 1
 
 def train(train_inputs, train_outputs, iterations):
-        for iteration in xrange(iterations):
- 
+        for iteration in range(iterations):
+
+            global synaptic_weights
+
             #getting output
-            output = getoutput(train_inputs)
+            x = dot(train_inputs, synaptic_weights)
+            output = activate(x)
 
          	#calculating error
             error = train_outputs - output
 
             #calculating the adjustment
-            adjustment = dot(train_inputs.T, error * sig_grad(output))
-            global synaptic_weights
+            adjustment = dot(train_inputs.T, error * activate_grad(x))
             synaptic_weights += adjustment
 
 
-def getoutput(inputs):
-        
-        return sigmoid(dot(inputs, synaptic_weights))
+def current_activator(x):
+    return tanh(x)
+
+def current_grad(x):
+    return tanh_grad(x)
+
+def activate(x):
+    fx = current_activator(x)
+
+    # return squared function if range is not positive
+    if (fx[0] > 0):
+        return fx
+    else:
+        return fx ** 2
+
+def activate_grad(x):
+    fx = current_activator(x)
+    dfx = current_grad(x)
+
+    # return squared function if range is not positive
+    if (dfx[0] > 0):
+        return dfx
+    else:
+        return fx * dfx * 2
 
 def sigmoid(x):
 	return 1 / (1 + exp(-x))
 
+def gaussian(x):
+    return exp(-(x ** 2))
+
 def sig_grad(x):
-        return x * (1 - x)
+    return sigmoid(x) * (1 - sigmoid(x))
+
+def gauss_grad(x):
+    return -(2 * x * gaussian(x))
+
+def softsign(x):
+    return x / (1 + abs(x))
+
+def softsign_grad(x):
+    return x / ((1 + abs(x)) ** 2)
+
+def tanh_grad(x):
+    return 1 - (tanh(x) ** 2)
 
 
-print "Random starting synaptic weights - "
-print synaptic_weights
+print("Random starting synaptic weights - ")
+print(synaptic_weights)
 
 
 train(train_inputs, train_outputs, 10000)
 
 
-print "synaptic weights after training: "
-print synaptic_weights
+print("synaptic weights after training: ")
+print(synaptic_weights)
 
 # Testing neural network 
-print "For testing output [1, 0, 0]:  "
-x= getoutput(array([1,1,0]))
-print x
+print("For testing output [1, 0, 0]:  ")
+fx = activate(dot(array([1,1,0]), synaptic_weights))
+print(fx)
